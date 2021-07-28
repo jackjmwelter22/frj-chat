d077be95ed0594ab039219704f0c3267d4368903
Jack Welter <jackjmwelter22@Jacks-MacBook-Pro.local>
2021-07-27 21:33:55 -0400

    Stubbing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import About from "./About";
+import Login from "./Login";
+import Register from "./Register";
+import Root from "./Root";
+import ContactUs from "./ContactUs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                Hello World
+            </div>
+            <Switch>
+                <Route path="/about">
+                    <About/>
+                </Route>
+                <Route path="/login">
+                    <Login/>
+                </Route>
+                <Route path="/register">
+                    <Register/>
+                </Route>
+                <Route path="/contactus">
+                    <ContactUs/>
+                </Route>
+                <Route path="/">
+                    <Root/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/ContactUs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class ContactUs extends React.Component {
+    render() {
+        return (<p>Contact Us </p>);
+    }
+}
